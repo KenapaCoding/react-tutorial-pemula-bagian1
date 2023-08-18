9022e0a3ee093d480c039a054b08b4fbc5a62c2f
ranggo pato <patoranggo@gmail.com>
2023-08-18 17:54:58 +0700

    challenge-1

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
 import React from 'react';
-import ProductCreate from './components/ProductCreate.js';
-import ProductList from './components/ProductList.js';
-import { Products } from './data/product.js';
+import ProductCard from './components/ProductCard';
 import './App.css';
-import { useState } from 'react';
 
 const App = () => {
-  const [products, setProducts] = useState(Products);
-  const editProductById = (id, data) => {
-    const updatedProducts = products.map((prod) => {
-      if (prod.id === id) {
-        console.log({ ...prod, ...data });
-        return { ...prod, ...data };
-      }
-
-      return prod;
-    });
-    console.log(updatedProducts);
-    setProducts(updatedProducts);
-  };
-  const onCreateProduct = (product) => {
-    setProducts([
-      ...products,
-      {
-        id: Math.round(Math.random() * 1998),
-        ...product,
-      },
-    ]);
-    console.log(products);
-  };
-  const onDeleteProduct = (id) => {
-    const updatedProducts = products.filter((prod) => {
-      return prod.id !== id;
-    });
-    setProducts(updatedProducts);
-  };
-
   return (
     <>
-      <div className="app-title">Belanja Mobil</div>
-      <ProductCreate onCreateProduct={onCreateProduct} />
-      <ProductList
-        onEditProduct={editProductById}
-        products={products}
-        onDeleteProduct={onDeleteProduct}
-      />
+      <ProductCard />
     </>
   );
 };
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,95 +1,23 @@
 import React from 'react';
-import { useState } from 'react';
-import { AiTwotoneEdit } from 'react-icons/ai';
-import { MdDeleteForever } from 'react-icons/md';
-import ProductEdit from './ProductEdit';
 
-const ProductCard = ({ product, onDeleteProduct, onEditProduct }) => {
-  const [jumlahProduct, setJumlahProduct] = useState(0);
-  const [showKeranjang, setShowKeranjang] = useState(true);
-  const [showEdit, setShowEdit] = useState(false);
-  const tambahProduct = (operator) => {
-    if (operator === '-') {
-      if (jumlahProduct === 1) {
-        setShowKeranjang(true);
-        setJumlahProduct((jumlahProduct) => jumlahProduct - 1);
-        return;
-      }
-      setJumlahProduct((jumlahProduct) => jumlahProduct - 1);
-    } else {
-      setJumlahProduct((jumlahProduct) => jumlahProduct + 1);
-    }
-  };
-  const handleKeranjang = () => {
-    setJumlahProduct((jumlahProduct) => jumlahProduct + 1);
-    setShowKeranjang(!showKeranjang);
-  };
-  const handleSubmitEdit = (id, data) => {
-    setShowEdit(false);
-    onEditProduct(id, data);
-  };
-  const handleCancelEdit = () => {
-    setShowEdit(false);
-  };
-
+const ProductCard = () => {
   return (
     <div className="card">
-      {showEdit ? (
-        <ProductEdit
-          product={product}
-          onEditProduct={handleSubmitEdit}
-          onCancel={handleCancelEdit}
-        />
-      ) : (
-        <>
-          <div className="edit-delete">
-            <AiTwotoneEdit
-              onClick={() => setShowEdit(!showEdit)}
-              className="icon-edit"
-            />
-            <MdDeleteForever
-              onClick={() => onDeleteProduct(product.id)}
-              className="icon-delete"
-            />
-          </div>
-          <img
-            src={product.imageURL}
-            alt="Avatar"
-            style={{
-              width: '100%',
-              height: '200px',
-              borderRadius: ' 10px 10px 0px 0px',
-            }}
-          />
-          <div className="container">
-            <h4>
-              <b>{product.nama}</b>
-            </h4>
-            <p>{product.deskripsi}</p>
-          </div>
-          <div
-            className={`card-keranjang ${
-              showKeranjang ? 'show-keranjang' : 'jumlah-product'
-            }`}
-          >
-            {showKeranjang ? (
-              <div className="keranjang" onClick={handleKeranjang}>
-                + Keranjang
-              </div>
-            ) : (
-              <>
-                <button className="button" onClick={() => tambahProduct('-')}>
-                  -
-                </button>
-                <div>{jumlahProduct}</div>
-                <button className="button" onClick={() => tambahProduct('+')}>
-                  +
-                </button>
-              </>
-            )}
-          </div>
-        </>
-      )}
+      <img
+        src="https://images.unsplash.com/photo-1551830820-330a71b99659?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MTQ2MDgwOQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        alt="Avatar"
+        style={{
+          width: '100%',
+          height: '200px',
+          borderRadius: ' 10px 10px 0px 0px',
+        }}
+      />
+      <div className="container">
+        <h4>
+          <b>Ford</b>
+        </h4>
+        <p>Mobil ford adalah mobil yang sangat bagus</p>
+      </div>
     </div>
   );
 };
